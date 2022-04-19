8828f67f230cb74303b2dcdab64af36f7c8fab09
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:16:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _63DersIndexers
+{
+    class Program
+    {
+        class Futbolcu
+        {
+            string[] futbolcular = new string[10]; // Indexers sayesinde futbolcularin isimlerini burada tutuyoruz.
+            public string this[int index]
+            {
+                get { return futbolcular[index]; }
+                set { futbolcular[index] = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Futbolcu IlkOnluGrup = new Futbolcu();
+            IlkOnluGrup[0] = "Murat";
+            IlkOnluGrup[1] = "Hasan";
+            IlkOnluGrup[2] = "Emre";
+            IlkOnluGrup[3] = "Yunus";
+            IlkOnluGrup[4] = "Kaan";
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(IlkOnluGrup[i]);
+            }
+        }
+    }
+}
